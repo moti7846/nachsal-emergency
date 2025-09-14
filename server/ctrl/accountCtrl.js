89de099debb4916a3ef1930ce8a64a55a4fa4df3
davidlandau770 <dal778770@gmail.com>
2025-09-14 16:09:58 +0300

    add middleware auth, changePassword ctrl

--- server/ctrl/accountCtrl.js
+++ server/ctrl/accountCtrl.js
@@ -1,5 +1,23 @@
-export const signup = () => {
+import { updateSoldierDB } from "../DAL/soldierDAL.js";
 
+export const changePassword = async (req, res) => {
+    const privateNumber = req.body.privateNumber;
+    const password = req.body.newPassword;
+    if (!privateNumber || !password) {
+        return res.status(403).json({ msg: "You must enter a personal number and password."})
+    }
+    let response;
+    try {
+        response = await updateSoldierDB(privateNumber, password);
+    }
+    catch (err) {
+        console.log(`updateSoldierDB: ${err.msg}`);
+        res.status(500).json({ msg: "Password change failed." })
+    }
+    if (!response) {
+        res.status(500).json({ msg: "User not found." })
+    }
+    res.json({ msg: "The update was successful!" })
 }
 
 export const login = () => {
