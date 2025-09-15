abf99b52dd5d3054d3ef222251c541a1b48d976a
davidlandau770 <dal778770@gmail.com>
2025-09-15 16:51:43 +0300

    add context auth

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,23 +6,46 @@ import Report_soldier_place from "./comp/Report_soldier_place";
 import Headbar from "./comp/Headbar";
 import Comoned_page from "./Pages/Comoned_page";
 import "./App.css"
+import { useEffect, useState } from "react";
+import { AuthContext, type Soldier } from "./context/authContext";
 
-
-export const URL = ""
-
-
+export const URL = "http://localhost:3000";
 
 export default function App() {
+  const [soldier, setSoldier] = useState<Soldier | null>(null);
+
+  const checkAuth = async () => {
+    try {
+      const res = await fetch(`${URL}/me`, { credentials: "include" });
+      if (res.ok) {
+        const data = await res.json();
+        setSoldier(data);
+      } else {
+        setSoldier(null);
+      }
+    } catch (err) {
+      console.error("Auth check failed:", err);
+      setSoldier(null);
+    };
+  }
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <>
-      <Headbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/report_soldier_place" element={<Report_soldier_place />} />
-        <Route path="/comoned" element={<Comoned_page/>}/>
-        <Route path="report_place" element={ <Report_place/>} />
-        <Route path="/login" element={<Login />} />
-      </Routes> 
+      <AuthContext.Provider value={{ soldier, setSoldier }}>
+        <Headbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/report_soldier_place" element={<Report_soldier_place />} />
+          <Route path="/comoned" element={<Comoned_page />} />
+          <Route path="report_place" element={<Report_place />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthContext.Provider>
+
     </>
   )
 }
