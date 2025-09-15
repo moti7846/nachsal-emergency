5d1649cf1c8a17d76490a377e1ebb39a10485b26
mkabg <m7110645k@gmail.com>
2025-09-15 09:22:27 +0300

    fix reset route

--- server/routes/configRoutes.js
+++ server/routes/configRoutes.js
@@ -1,11 +1,11 @@
 import { soldiers } from "./accountRoute.js"
 import { presence } from "./presenceRoute.js"
-import { resetReport} from "./resetReport.js"
+import { nachsal} from "./resetNachsal.js"
 
 const configRoutes = (app) => {
     app.use('/auth', soldiers)
     app.use('/presence', presence)
-    app.use('/nachsal', resetReport)
+    app.use('/nachsal', nachsal)
 }
 
 export {
