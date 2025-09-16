fb0d991c5ea114be141957d50b435244d75e2bab
israelr9897 <israelr9897@gmail.com>
2025-09-16 17:55:12 +0300

    Adding routing and a function to create a report in the database

--- server/ctrl/reportCtrl.js
+++ server/ctrl/reportCtrl.js
@@ -1,30 +1,33 @@
-import { getDirectSoldiersDB, getDirectSoldiersWithReportsDB } from "../DAL/soldierDAL.js";
+import { createReportDB } from "../DAL/reportDAL.js";
+import {
+  getDirectSoldiersDB,
+  getDirectSoldiersWithReportsDB,
+} from "../DAL/soldierDAL.js";
 import { mapSoldiers } from "../services/mapSoldires.js";
 
-
 export const getDirectSoldiers = async (req, res) => {
-    const personalNumber = req.params.personalNumber;
-    let response;
-    try {
-        response = await getDirectSoldiersDB(personalNumber);
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({ msg: "The request failed." })
-    }
-    res.json(response);
-}
+  const personalNumber = req.params.personalNumber;
+  let response;
+  try {
+    response = await getDirectSoldiersDB(personalNumber);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ msg: "The request failed." });
+  }
+  res.json(response);
+};
 
 export const getDirectSoldiersWithReports = async (req, res) => {
-    const personalNumber = req.params.personalNumber;
-    let response;
-    try {
-        response = await getDirectSoldiersWithReportsDB(personalNumber);
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({ msg: "The request failed." })
-    }
-    res.json(response);
-}
+  const personalNumber = req.params.personalNumber;
+  let response;
+  try {
+    response = await getDirectSoldiersWithReportsDB(personalNumber);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ msg: "The request failed." });
+  }
+  res.json(response);
+};
 
 export const SendNachsal = async (req, res) => {
   const arraySoldires = await mapSoldiers(req.params.personalNumber);
@@ -32,13 +35,24 @@ export const SendNachsal = async (req, res) => {
 };
 
 export const getSoldierDetails = async (req, res) => {
-    const personalNumber = req.params.personalNumber;
-    let response;
-    try {
-        response = await getSoldierByIdDB(personalNumber);
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({ msg: "The request failed." })
-    }
-    res.json(response);
-}
+  const personalNumber = req.params.personalNumber;
+  let response;
+  try {
+    response = await getSoldierByIdDB(personalNumber);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ msg: "The request failed." });
+  }
+  res.json(response);
+};
+
+export const createReport = async (req, res) => {
+  try {
+    const isTrue = await createReportDB({ ...req.body, done: true }, req.params.personalNumber);
+    if (isTrue) return res.json({ msg: "√ added report" });
+    else res.status(403).json({ msg: "create report failed" });
+  } catch (error) {
+    console.log("create Report error: ", error);
+    res.status(500).json({ msg: "create report failed" });
+  }
+};
