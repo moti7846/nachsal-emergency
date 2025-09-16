d5c1a2166dde3229dc073263eefd94467056cc97
israelr9897 <israelr9897@gmail.com>
2025-09-16 12:17:43 +0300

    Adding a function that maps all indirect commands by command number, as well as routing to run the following function.

--- server/ctrl/reportCtrl.js
+++ server/ctrl/reportCtrl.js
@@ -1,13 +1,19 @@
 import { getDirectSoldiersDB } from "../DAL/soldierDAL.js";
+import { mapSoldiers } from "../services/mapSoldires.js";
 
 export const getDirectSoldiers = async (req, res) => {
-    const personalNumber = req.params.personalNumber;    
-    let response;
-    try {
-        response = await getDirectSoldiersDB(personalNumber);
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({ msg: "The request failed." })
-    }
-    res.json(response);
-}
+  const personalNumber = req.params.personalNumber;
+  let response;
+  try {
+    response = await getDirectSoldiersDB(personalNumber);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ msg: "The request failed." });
+  }
+  res.json(response);
+};
+
+export const SendNachsal = async (req, res) => {
+  const arraySoldires = await mapSoldiers(req.params.personalNumber);
+  res.send(arraySoldires);
+};
--- server/routes/reportRoute.js
+++ server/routes/reportRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { checkAuth } from "../middelware/auth.js";
-import { getDirectSoldiers } from "../ctrl/reportCtrl.js";
+import { getDirectSoldiers, SendNachsal } from "../ctrl/reportCtrl.js";
 
 const report = express.Router();
 
 report.use(checkAuth);
 report.get("/:personalNumber", getDirectSoldiers);
+report.get("/send_nachsal/:personalNumber", SendNachsal);
 
 export default report;
