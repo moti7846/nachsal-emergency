33aef47fbdd15453a637e7b674db2f1117699318
moti7846 <8563816@gmail.com>
2025-09-16 17:58:02 +0300

    Add title attribute to logout icon and adjust hover effects for account image

--- front/src/comp/top nav/TopNav.tsx
+++ front/src/comp/top nav/TopNav.tsx
@@ -6,7 +6,7 @@ export default function TopNav() {
       <div className="nav-top">
         <img src="/IDF.png" alt="לוגו של צהל" className="logo-idf" />
         <h2 className="slogen-idf">מערכת נכס"ל צה"ל</h2>
-        <Link to={'logout'}><img className="account-img" src="account.png" alt="אייקון חשבון" /></Link>
+        <Link to={'logout'}><img title="התנתק" className="account-img" src="account.png" alt="אייקון חשבון" /></Link>
       </div>
     </>
   );
