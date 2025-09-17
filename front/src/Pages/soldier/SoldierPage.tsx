ffd8e5c21971544865fa576b200359c0c7bfa329
davidlandau770 <dal778770@gmail.com>
2025-09-17 15:08:37 +0300

    add inport params

--- front/src/Pages/soldier/SoldierPage.tsx
+++ front/src/Pages/soldier/SoldierPage.tsx
@@ -2,6 +2,7 @@ import "./soldierPage.css"
 
 import SoldierTable from "../../comp/soldier/SoldierTable";
 import PersonalData from "../../comp/personalData/PersonalData";
+import { useParams } from "react-router";
 export default function SoldierPage() {
   const params = useParams();
   return (
